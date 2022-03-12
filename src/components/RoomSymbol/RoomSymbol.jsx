05b57b2b48c228a6911627aee4225de043034882
Jared Tessensohn <jtessensohn@yahoo.com>
2022-03-12 00:07:21 -0600

    feat: basic RoomSymbol

--- src/components/RoomSymbol/RoomSymbol.jsx
+++ src/components/RoomSymbol/RoomSymbol.jsx
@@ -1,5 +1,16 @@
+import styles from './RoomSymbol.module.css'
+import witnessImg from '../../assets/witness.png';
+
 export const RoomSymbol = () => {
   return(
-    <p>RoomSymbol</p>
+    <div className={styles.cell}>
+      <div className={styles.top}>
+        <img src={witnessImg} alt="" />
+      </div>
+      <div className={styles.bottom}>
+        <img src={witnessImg} alt="" />
+      </div>
+    </div>
+
   );
 };
