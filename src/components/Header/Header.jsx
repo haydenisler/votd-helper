5807b1df8fabd22a48f7f52238fc714e2a65bbe0
Hayden Isler <me@haydenisler.com>
2022-03-13 01:51:38 -0600

    style: header layout + styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import styles from './Header.module.css';
+import raidImg from '../../assets/raid.png';
+import fleetImg from '../../assets/witness.png';
 
 export const Header = () => {
   return(
-    <nav className={styles.header}>
-      <h1 className={styles.logo}>VotD Helper</h1>
+    <nav className={styles['header']}>
+      <div 
+        className={styles['logo-container']} 
+        style={{ backgroundImage: `linear-gradient(135deg, transparent 60%, #292524 80%, #292524 100%), url(${fleetImg})`}}>
+        <img src={raidImg} className={styles['logo-image']}/>
+        <h1 className={styles['logo-text']}>VOTD HELPER</h1>
+      </div>
     </nav>
   );
 };
