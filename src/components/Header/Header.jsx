0fa35c1f8bea8b052ef5621df295f86b98aba913
Hayden Isler <me@haydenisler.com>
2022-03-14 00:11:32 -0500

    fix: remove header text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,6 @@ export const Header = () => {
         className={styles['logo-container']} 
         style={{ backgroundImage: `linear-gradient(135deg, transparent 60%, #292524 80%, #292524 100%), url(${fleetImg})`}}>
         <img src={raidImg} className={styles['logo-image']}/>
-        <h1 className={styles['logo-text']}>VOTD HELPER</h1>
       </div>
     </nav>
   );
