bfdf402c88ae2b27e24529fe6042de74ea345e1a
Hayden Isler <me@haydenisler.com>
2022-03-14 00:19:30 -0500

    feat: add WallSymbols boilerplate

---
+++ src/components/WallSymbols/WallSymbols.jsx
@@ -0,0 +1,5 @@
+export const WallSymbols = () => {
+  return(
+    <div></div>  
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Header';
 export * from './RoomSymbol';
 export * from './RoomSymbols';
+export * from './WallSymbols';
